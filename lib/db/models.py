8957f0ab8da5d7c3ea31363438a9a2c89f073f8c
Lauren Slayman <laurenaslayman@gmail.com>
2023-05-16 16:26:01 -0600

    creates table outlines

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,3 +1,34 @@
 #this file will contain db tables, operations & data validation
 
-print("Hello, world!")
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+convention = {
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+}
+metadata = MetaData(naming_convention=convention)
+
+Base = declarative_base(metadata=metadata)
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer(), primary_key=True)
+    username = Column(String())
+    score = Column(Integer())
+
+class Achievement(Base):
+    __tablename__ = 'achievements'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    description = Column(String())
+    minimum_score = Column(Integer())
+
+class UserAchievement(Base):
+    __tablename__ = 'user_achievements'
+
+    id = Column(Integer(), primary_key=True)
+    user_id = Column(Integer(), ForeignKey('users.id'))
+    achievement_id = Column(Integer(), ForeignKey('achievements.id'))
